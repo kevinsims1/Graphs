718a0283c2a1896f823a6fbf7d0df0e989377508
kevinsims1 <kevinsims1@outlook.com>
2020-01-21 17:13:01 -0600

    init

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,14 @@
-
+from graph import Graph
+sequence = []
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    for pairs in ancestors:
+        if pairs[1] == starting_node:
+            sequence.append(pairs[1])
+            print(sequence)
+            earliest_ancestor(ancestors, pairs[0])
+        
+    return sequence
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(test_ancestors, 1))
